6e40c2b81b6c1656c0f93e6a07a51f6616dbc6bc
hrid1 <hridoycse1@gmail.com>
2025-04-06 23:57:07 +0600

    p1 Done

--- models/Otp.js
+++ models/Otp.js
@@ -16,4 +16,23 @@ const otpSchema = new mongoose.Schema({
   },
 });
 
+// function --> to send emails
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email form Study-Notion",
+      otp
+    );
+    console.log("Email send successfully", mailResponse);
+  } catch (error) {
+    console.log("Error Ocured while sending mail", error);
+    throw error;
+  }
+}
+
+otpSchema.pre("save", async function (next) {
+  await sendVerificationEmail(this.email, this.otp);
+  next();
+});
 module.exports = mongoose.model("OTP", otpSchema);
